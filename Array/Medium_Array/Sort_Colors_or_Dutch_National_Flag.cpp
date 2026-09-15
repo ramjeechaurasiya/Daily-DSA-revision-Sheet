f1da6acf59bee8ed07340f76d3535ad3f7e259a4
YourGitHubUsername <rk7007872@gmail.com>
2026-09-15 22:40:46 +0530

    implementing Dutch flag algorithm after learnig

--- Array/Medium_Array/Sort_Colors_or_Dutch_National_Flag.cpp
+++ Array/Medium_Array/Sort_Colors_or_Dutch_National_Flag.cpp
@@ -7,27 +7,57 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>nums[i];
     }
-    int red=0;
-        int white=0;
-        int blue=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0)red++;
-            if(nums[i]==1)white++;
-            if(nums[i]==2)blue++;
-        }
-        int idx=0;
-        while(red--){
-            nums[idx++]=0;
-        }
-        while(white--){
-            nums[idx++]=1;
-        }
-        while(blue--){
-            nums[idx++]=2;
-        }
+    // int red=0;
+    //     int white=0;
+    //     int blue=0;
+    //     for(int i=0;i<n;i++){
+    //         if(nums[i]==0)red++;
+    //         if(nums[i]==1)white++;
+    //         if(nums[i]==2)blue++;
+    //     }
+    //     int idx=0;
+    //     while(red--){
+    //         nums[idx++]=0;
+    //     }
+    //     while(white--){
+    //         nums[idx++]=1;
+    //     }
+    //     while(blue--){
+    //         nums[idx++]=2;
+    //     }
 
-        for(int i=0;i<n;i++){
-            cout<<nums[i]<<" ";
+    //     for(int i=0;i<n;i++){
+    //         cout<<nums[i]<<" ";
+    //     }
+    //     return 0;
+
+
+
+
+    // Dutch flag algorithm
+
+    int low=0;
+    int mid=0;
+    int high=n-1;
+    while(mid<=high){
+        if(nums[mid]==0){
+            swap(nums[low],nums[mid]);
+            low++;
+            mid++;
         }
-        return 0;
+        else if(nums[mid]==1){
+            mid++;
+        }
+        else{
+            swap(nums[mid],nums[high]);
+            high--;
+        }
+    }
+for(int i=0;i<n;i++){
+    cout<<nums[i]<<" ";
+
+}
+cout<<endl;
+return 0;
+
 }
